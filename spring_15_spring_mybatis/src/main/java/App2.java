222e0c357cd9c4f3b54f9c9c762363bdd4f0aa1f
申诺呦 <shennuo123@163.com>
2023-12-07 20:15:47 +0800

    Spring整合MyBatis

---
+++ spring_15_spring_mybatis/src/main/java/App2.java
@@ -0,0 +1,16 @@
+import com.example.config.SpringConfig;
+import com.example.domain.Account;
+import com.example.service.AccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App2 {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        AccountService accountService = ctx.getBean(AccountService.class);
+
+        Account ac = accountService.findById(1);
+        System.out.println(ac);
+    }
+}
